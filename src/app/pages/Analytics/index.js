5424c85ab5113aa47f57a33c597ad9a242329c8c
AdamKniec <adam.k.kniec@gmail.com>
2018-11-24 18:58:08 +0100

    fixing console error

--- src/app/pages/Analytics/index.js
+++ src/app/pages/Analytics/index.js
@@ -15,13 +15,13 @@ class Analytics extends Component {
         // }).then(data => {
         //     console.log(data);
         // })
-        fetch('https://0f9gctnbb6.execute-api.eu-central-1.amazonaws.com/hackyeah-eam/get-data')
-           .then(function (response) {
-               return response.json();
-           })
-           .then(function (myJson) {
-               console.log(JSON.stringify(myJson));
-           });
+        // fetch('https://0f9gctnbb6.execute-api.eu-central-1.amazonaws.com/hackyeah-eam/get-data')
+        //    .then(function (response) {
+        //        return response.json();
+        //    })
+        //    .then(function (myJson) {
+        //        console.log(JSON.stringify(myJson));
+        //    });
     }
     render() {
         return (
