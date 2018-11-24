5634a11aea55dcc2c550a3d4bf11fb441bf9034e
AdamKniec <adam.k.kniec@gmail.com>
2018-11-25 00:25:41 +0100

    fixing the conflict

--- src/app/pages/Analytics/AnalyticsOne/index.js
+++ src/app/pages/Analytics/AnalyticsOne/index.js
@@ -16,7 +16,6 @@ class AnalyticsOne extends Component {
 
    componentDidMount() {
 
-<<<<<<< HEAD
         const url = "https://0f9gctnbb6.execute-api.eu-central-1.amazonaws.com/hackyeah-eam/get-data?wojewodztwo=05";
         fetch(url)
             .then(function (response) {
@@ -27,18 +26,6 @@ class AnalyticsOne extends Component {
             });
     }
     render() {
-=======
-       const url = "https://0f9gctnbb6.execute-api.eu-central-1.amazonaws.com/hackyeah-eam/get-data?wojewodztwo=05";
-       fetch(url)
-           .then(function (response) {
-               return response.json();
-           })
-           .then(function (myJson) {
-               console.log(JSON.stringify(myJson));
-           });
-   }
-   render() {
->>>>>>> 1d896dd9277a4061353d9a405e078adf395b1f2c
 
        return (
            <div className="analytics-heading">
