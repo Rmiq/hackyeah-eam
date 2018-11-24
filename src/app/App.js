1400ab8e14a794c20f16cdd5679f82cc93b4dd31
AdamKniec <adam.k.kniec@gmail.com>
2018-11-24 14:42:24 +0100

    appJS after pull sync

--- src/app/App.js
+++ src/app/App.js
@@ -12,13 +12,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-<<<<<<< HEAD
           <Header></Header>
           <div class="sideNav"></div>
-=======
-          <div className="topNav"></div>
-          <div className="sideNav"></div>
->>>>>>> 472331c13ed108e07d7c332b33566c042ad4712f
         </header>
         <main>
           <Router>
