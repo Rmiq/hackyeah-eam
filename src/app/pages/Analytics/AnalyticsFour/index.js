722de34dc25db5c921b1f1b1dafed84193116a73
Remigiusz Chiluta <remigiusz.x.chiluta@gsk.com>
2018-11-25 04:29:03 +0100

    $@

--- src/app/pages/Analytics/AnalyticsFour/index.js
+++ src/app/pages/Analytics/AnalyticsFour/index.js
@@ -24,40 +24,6 @@ class AnalyticsFour extends Component {
     }
 
     render() {
-<<<<<<< HEAD
-=======
-        const data = [
-            {
-                name: 'Page A',
-                pv: 2400,
-                amt: 2400
-            }, {
-                name: 'Page B',
-                pv: 1398,
-                amt: 2210
-            }, {
-                name: 'Page C',
-                pv: 9800,
-                amt: 2290
-            }, {
-                name: 'Page D',
-                pv: 3908,
-                amt: 2000
-            }, {
-                name: 'Page E',
-                pv: 4800,
-                amt: 2181
-            }, {
-                name: 'Page F',
-                pv: 3800,
-                amt: 2500
-            }, {
-                name: 'Page G',
-                pv: 4300,
-                amt: 2100
-            }
-        ];
->>>>>>> 81a4fc99d43c28f63899148a822ba93388e73ba0
 
         return (
             <div className="analytics-heading">
@@ -70,12 +36,8 @@ class AnalyticsFour extends Component {
                     <YAxis/>
                     <Tooltip/>
                     <Legend />
-<<<<<<< HEAD
                     <Bar dataKey="zapytania" name="Porzucone" stackId="a" fill="#82ca9d" />
                     <Bar dataKey="sfinalizowane" name="Umówione" stackId="a" fill="#8884d8" />
-=======
-                    <Bar dataKey="pv" fill="#8884d8" />
->>>>>>> 81a4fc99d43c28f63899148a822ba93388e73ba0
                 </BarChart>
                 <div className="chart-legend"> 
                     <h4> Legenda: </h4>
