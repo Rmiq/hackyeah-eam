f5c3305dc5d21b62348cf91294aeb2768ede2400
Remigiusz Chiluta <remigiusz.x.chiluta@gsk.com>
2018-11-25 07:37:36 +0100

    $@

--- src/app/components/TableData/index.js
+++ src/app/components/TableData/index.js
@@ -45,7 +45,6 @@ function PlacesTable(props) {
                     <TableRow>
                         <CustomTableCell>Nazwa</CustomTableCell>
                         <CustomTableCell>Lokalizacja</CustomTableCell>
-                        <CustomTableCell>Ulica</CustomTableCell>
                         <CustomTableCell>Dostępność</CustomTableCell>
                     </TableRow>
                 </TableHead>
@@ -56,8 +55,7 @@ function PlacesTable(props) {
                                 <CustomTableCell key={x.attributes.id} component="th" scope="row">
                                     {x.attributes.provider}
                                 </CustomTableCell>
-                                <CustomTableCell key={x.attributes.id} numeric>{x.attributes.locality}</CustomTableCell>
-                                <CustomTableCell key={x.attributes.id} numeric>{x.attributes.address}</CustomTableCell>
+                                <CustomTableCell key={x.attributes.id} numeric>{`${x.attributes.locality}, ${x.attributes.address}`}</CustomTableCell>
                                 <CustomTableCell key={x.attributes.id} numeric>{x.attributes.dates.date}</CustomTableCell>
 
                             </TableRow>
